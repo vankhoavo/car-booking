a0f0943c6570a100880cdb27b353c1eae7a29405
Võ Văn Khoa <vankhoa.work@gmail.com>
2026-09-07 16:48:39 +0700

    feat: expose public booking rental and blog routes

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,21 @@
 <?php
 
+use App\Http\Controllers\BlogController;
+use App\Http\Controllers\BookingController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\RentalController;
 use App\Http\Controllers\Teams\TeamInvitationController;
 use App\Http\Middleware\EnsureTeamMembership;
 use Illuminate\Support\Facades\Route;
 
 Route::inertia('/', 'Welcome')->name('home');
-Route::inertia('/dat-xe', 'booking/Index')->name('booking.index');
-Route::inertia('/thue-xe', 'rental/Index')->name('rental.index');
-Route::inertia('/tour', 'tour/Index')->name('tour.index');
+Route::get('/dat-xe', [BookingController::class, 'create'])->name('booking.index');
+Route::post('/dat-xe', [BookingController::class, 'store'])->name('booking.store');
+Route::get('/thue-xe', [RentalController::class, 'create'])->name('rental.index');
+Route::post('/thue-xe', [RentalController::class, 'store'])->name('rental.store');
+Route::get('/tour', fn () => redirect('/'))->name('tour.index');
+Route::get('/blog', [BlogController::class, 'index'])->name('blog.index');
+Route::get('/blog/{slug}', [BlogController::class, 'show'])->name('blog.show');
 
 Route::prefix('{current_team}')
     ->middleware(['auth', 'verified', EnsureTeamMembership::class])
