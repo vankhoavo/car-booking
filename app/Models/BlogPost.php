3eec695a8947e5b5045c2b3dd9ef51500018cf6d
Võ Văn Khoa <vankhoa.work@gmail.com>
2026-09-07 20:42:07 +0700

    fix: tighten blog model static analysis

--- app/Models/BlogPost.php
+++ app/Models/BlogPost.php
@@ -2,13 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class BlogPost extends Model
 {
-    use HasFactory;
-
     protected $fillable = [
         'title', 'slug', 'excerpt', 'content', 'image', 'published_at', 'is_published',
     ];
