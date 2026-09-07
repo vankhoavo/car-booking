a3c099ce3ef871eb14f3e6663cb72db44630fb64
Võ Văn Khoa <vankhoa.work@gmail.com>
2026-09-07 16:48:28 +0700

    feat: add public blog pages

---
+++ app/Http/Controllers/BlogController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\BlogPost;
+use Inertia\Inertia;
+use Inertia\Response;
+
+class BlogController extends Controller
+{
+    public function index(): Response
+    {
+        return Inertia::render('blog/Index', [
+            'posts' => BlogPost::query()
+                ->where('is_published', true)
+                ->orderByDesc('published_at')
+                ->orderByDesc('id')
+                ->get(['id', 'title', 'slug', 'excerpt', 'image', 'published_at']),
+        ]);
+    }
+
+    public function show(string $slug): Response
+    {
+        $post = BlogPost::query()
+            ->where('is_published', true)
+            ->where('slug', $slug)
+            ->firstOrFail();
+
+        return Inertia::render('blog/Show', ['post' => $post]);
+    }
+}
