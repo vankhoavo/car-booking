76c6732f72cea0edf4b3ed82e34ca593198ff317
Võ Văn Khoa <vankhoa.work@gmail.com>
2026-09-07 16:48:16 +0700

    feat: validate public rental requests

---
+++ app/Http/Requests/StoreRentalRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreRentalRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    protected function prepareForValidation(): void
+    {
+        $this->merge([
+            'phone' => preg_replace('/\s+/', '', (string) $this->input('phone')),
+        ]);
+    }
+
+    public function rules(): array
+    {
+        return [
+            'customer_name' => ['required', 'string', 'max:120'],
+            'phone' => ['required', 'regex:/^(0|\+84)(3|5|7|8|9)[0-9]{8}$/'],
+            'email' => ['required', 'email:rfc,dns', 'max:190'],
+            'vehicle_id' => ['required', 'integer', 'exists:vehicles,id'],
+            'start_date' => ['required', 'date', 'after_or_equal:today'],
+            'end_date' => ['required', 'date', 'after_or_equal:start_date'],
+            'pickup_location' => ['required', 'string', 'max:255'],
+            'return_location' => ['required', 'string', 'max:255'],
+            'passengers' => ['required', 'integer', 'min:1', 'max:60'],
+            'notes' => ['nullable', 'string', 'max:2000'],
+        ];
+    }
+}
