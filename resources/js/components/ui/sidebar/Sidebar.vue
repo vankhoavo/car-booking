feafd026cab27d1079d1664f8a9d1bc402bdc430
Võ Văn Khoa <vankhoa.work@gmail.com>
2026-09-08 20:19:55 +0700

    fix: align desktop sidebar markup

--- resources/js/components/ui/sidebar/Sidebar.vue
+++ resources/js/components/ui/sidebar/Sidebar.vue
@@ -1,4 +1,4 @@
-<script setup lang="ts"
+<script setup lang="ts">
 import type { HTMLAttributes } from "vue"
 import { cn } from "@/lib/utils"
 import { Sheet, SheetContent } from '@/components/ui/sheet'
