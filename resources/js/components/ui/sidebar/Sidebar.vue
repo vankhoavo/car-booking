56ccf530bcaa904cba50d32f10c951bb5ced307a
Võ Văn Khoa <vankhoa.work@gmail.com>
2026-09-08 08:47:05 +0700

    fix: remove md:block from sidebar wrapper

--- resources/js/components/ui/sidebar/Sidebar.vue
+++ resources/js/components/ui/sidebar/Sidebar.vue
@@ -53,14 +53,14 @@ const { isMobile, state, openMobile, setOpenMobile } = useSidebar()
 
   <div
     v-else
-    class="group peer text-sidebar-foreground hidden md:block"
+    class="group peer text-sidebar-foreground"
     data-slot="sidebar"
     :data-state="state"
     :data-collapsible="state === 'collapsed' ? collapsible : ''"
     :data-variant="variant"
     :data-side="side"
   >
-    <!-- This is what handles the sidebar gap on desktop  -->
+    <!-- This is what handles the sidebar gap on desktop -->
     <div
       :class="cn(
         'relative w-(--sidebar-width) bg-transparent transition-[width] duration-200 ease-linear',
@@ -77,7 +77,6 @@ const { isMobile, state, openMobile, setOpenMobile } = useSidebar()
         side === 'left'
           ? 'left-0 group-data-[collapsible=offcanvas]:left-[calc(var(--sidebar-width)*-1)]'
           : 'right-0 group-data-[collapsible=offcanvas]:right-[calc(var(--sidebar-width)*-1)]',
-        // Adjust the padding for floating and inset variants.
         variant === 'floating' || variant === 'inset'
           ? 'p-2 group-data-[collapsible=icon]:w-[calc(var(--sidebar-width-icon)+(--spacing(4))+2px)]'
           : 'group-data-[collapsible=icon]:w-(--sidebar-width-icon) group-data-[side=left]:border-r group-data-[side=right]:border-l',
